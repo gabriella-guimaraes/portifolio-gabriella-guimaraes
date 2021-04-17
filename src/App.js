7ddad170f48278861650a74ab26206f46683d2f8
gabriella-guimaraes <gabigara13@gmail.com>
2021-04-17 17:05:41 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Página em construção...
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/gabriella-guimaraes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
